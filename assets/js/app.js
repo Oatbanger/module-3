ba8a89acf59b74d3b2e963758fbacf6cb2a3bc1e
Cameron Nord <cnord6@gmail.com>
2021-11-13 12:04:39 -0700

    add password gen function

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,33 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword () {
+  // window prompt to set length
+ var password = window.prompt("How many characters would you like your password to contain?")
+  console.log (password);
+   //validate answer
+   if (password > 7 && password < 129) {
+     //generate password function
+     function foo () {};
+   }
+     // try again
+    else {
+      window.alert("Please enter a valid number between 8 and 128")
+      generatePassword();
+      ;
+    }
+   
+  // window confirm to set lower case type
+  var caseTypeLower = window.confirm("Include lower case?")
+  console.log (caseTypeLower);
+   
+  // window confirm to set upper case type
+  var caseTypeUpper = window.confirm("Include upper case?")
+  console.log (caseTypeUpper);
+  // window confirm to set special char allowance
+  var specialChar = window.confirm("Include special characters?")
+  console.log (specialChar);
+}
 
 // Write password to the #password input
 function writePassword() {
